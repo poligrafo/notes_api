789f437bc66710d220c95797df594da394b09725
poligrafo <poligrafo888@gmail.com>
2025-02-18 11:48:13 +0300

    features/add alembic conf

--- src/app/db/session.py
+++ src/app/db/session.py
@@ -2,8 +2,9 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from app.core.config import settings
 
+DATABASE_URL = settings.get_db_url()
 
-engine = create_async_engine(settings.get_db_url(), echo=False, future=True)
+engine = create_async_engine(url=DATABASE_URL, echo=False, future=True)
 
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
